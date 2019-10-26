9b659c0937b8e0b93abb0b66e590ff815a32ca6b
Paulo Gonçalves Lima <paulogoncalves436@gmail.com>
2019-10-26 14:07:50 -0300

    #55 - Concertando o funcionamento do bot
    Co-authored-by: Pedro Cella

--- Rasa/bot/actions/start.py
+++ Rasa/bot/actions/start.py
@@ -11,27 +11,21 @@ class StartComand(Action):
 
     def run(self, dispatcher, tracker, domain):
         try:
-            tracker = tracker.current_state()
-            sender_id = tracker['sender_id']
+        
+            db = MongoClient("mongodb://mongo:27017")
+            TelegramDB = db.TelegramDB
 
-            text = ('Espera uns segundinhos aí, rapidinho... ')
-            sleep(2.75)
-
-            data = requests.get(
-                'https://api.telegram.org/bot{}/sendMessage?chat_id={}&text={}'
-                .format(TELEGRAM_TOKEN, sender_id, text)
-            ).json()
-
-            client = MongoClient(TELEGRAM_DB)
-            db = client['telegram_db']
-
-            users_data = list(db.users.find({}, {'sender_id': 1, '_id': 0}))
-
-            users_id = []
-            for users in users_data:
-                users_id.append(users['sender_id'])
-
-            print(users_data)
+            user_data = {
+                'name': 'First Name',
+                'last_name': 'last_name',
+                'materias': {
+                    'materia1': 'nome_da_primeira',
+                    'materia2': 'nome_da_segunda',
+                    'materia3': 'nome_da_terceira'
+                }
+            }
+            result = TelegramDB.insert_one(user_data)
+            print('One user: {0}'.format(result.inserted_id))
 
             # if sender_id in users_id:
             #     # User found in the database
@@ -43,5 +37,5 @@ class StartComand(Action):
             #         new_data = self.build_telegram_user(data, sender_id)
             #         self.save_telegram_user(new_data, db)
 
-        except ValueError:
-            dispatcher.utter_message(ValueError)
+        except Exception as exc:
+            dispatcher.utter_message(exc)
