8c4f40003cfadf0d31d4113f7efb9fcf63465cf4
Paulo Gonçalves Lima <paulogoncalves436@gmail.com>
2019-11-11 17:12:01 -0300

    Adicionando o /start para pegar e salvar o ID do usuario.

--- bot/actions/start.py
+++ bot/actions/start.py
@@ -11,5 +11,24 @@ class ActionStart(Action):
             client = MongoClient("mongo:27017")
             db = client.telegramdb
             collectionsUsers = db.user
+
+            tracker = tracker.current_state()
+            sender_id = tracker['sender_id']
+            
+            if not collectionsUsers.count({'SenderID': sender_id}):
+                first_name = None
+                last_name = None
+                user = {
+                    'SenderID': sender_id,
+                    'first_name': first_name,
+                    'last_name': last_name,
+                    'classes': {},
+                    'exams': {},
+                    'activities': {}
+                }
+                collectionsUsers.insert_one(user)
+            
+
+
         except ValueError:
             dispatcher.utter_message(ValueError)
