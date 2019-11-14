30997d704b67ccf03340e55d2e2234b5cb0d7abf
Pedro Cella <pedrovscella@gmail.com>
2019-11-14 01:02:05 -0300

    Adição do comando validate no makefile do bot, intents desnecessárias apagadas e descomentar o arquivo de materia.py para teste do CI

--- bot/actions/materia.py
+++ bot/actions/materia.py
@@ -1,20 +1,20 @@
-# from typing import Any, Text, Dict, List
+from typing import Any, Text, Dict, List
 
-# from rasa_sdk import Action, Tracker
-# from rasa_sdk.executor import CollectingDispatcher
-# from rasa_sdk.events import SlotSet
+from rasa_sdk import Action, Tracker
+from rasa_sdk.executor import CollectingDispatcher
+from rasa_sdk.events import SlotSet
 
 
-# class ActionMateria(Action):
-#     def name(self):
-#         return "action_materia"
+class ActionMateria(Action):
+    def name(self):
+        return "action_materia"
 
-#     def run(self, dispatcher: CollectingDispatcher, tracker: Tracker, domain: Dict[Text, Any]):
+    def run(self, dispatcher: CollectingDispatcher, tracker: Tracker, domain: Dict[Text, Any]):
 
-#         materia = tracker.get_slot('materia')
+        materia = tracker.get_slot('materia')
 
-#         try:
-#             dispatcher.utter_message("Você está fazendo {}?".format(materia))
-#         except ValueError:
-#             dispatcher.utter_message(ValueError)
-#         return [SlotSet("materia", materia)]
+        try:
+            dispatcher.utter_message("Você está fazendo {}?".format(materia))
+        except ValueError:
+            dispatcher.utter_message(ValueError)
+        return [SlotSet("materia", materia)]
